af3ea72b3b184bf729108ba953b7206c6bb52b93
gnbuck <gnbuck@gnbuck.nomail>
2020-12-12 23:45:25 +0100

    improve README

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# poe_quality_batches
-Python script to compute the best vendor receipe batches for obtaining items like  Gemcutter's Prism or  Glassblower's Bauble
+# Poe Quality Batches
+
+## Description
+
+This tool is a Python script to compute the best vendor receipe batches for obtaining items like Gemcutter's Prism or Glassblower's Bauble.
+
+It is able to query the inventory of a character via Path Of Exile [official website](https://www.pathofexile.com/) and to tell you how to get the most possible of those currencies. Alternatively and for historical reason, it is able to compute the result via a [hardcoded list](poe_quality_batches/samples.py) of quality values
+
+## Prerequisites
+
+- Python 3.8
+- Make
+
+## Usage
+
+- Create environnement
+
+``` shell
+make env
+```
+
+- Run in online mode
+
+``` shell
+make run
+```
+
+- Run in offline mode
+
+``` shell
+make run
+```
+
+## Contributing
+
+If you want to contribute to this project, do not hesitate to open a pull request.
+
+- Create dev environnement
+
+``` shell
+make env-dev
+```
+
+- Run offline with debug enabled (logs into /out.txt)
+``` shell
+make run-debug
--- poe_quality_batches/quality_batches.py
+++ poe_quality_batches/quality_batches.py
@@ -1,7 +1,7 @@
 import sys
 
 from .core.runner import runner
-from .helpers import find_uniques
+from .helpers import find_uniques, compute_values_from_indexes
 from .http.client import client
 from .samples import SAMPLES
 from .settings import LIMIT as limit
@@ -18,29 +18,30 @@ def main(online, debug):
     items = sorted(items, reverse=True)
 
     # 'uniques' is a list of tuples, where each tuple equal the index of the first
-    # occurence of a value, and the value corresponding to this index.
-    # This helps to reduce the number of time the recursive method is run
+    # occurence of a value, and the value corresponding to this index. It helps to
+    # reduce the number of time the recursive method is run.
     uniques = find_uniques(items)
 
     sys.setrecursionlimit(6000)
     original_stdout = sys.stdout
 
-    # Following two blocks print the result either in a file or in the stdout, depending
-    # on personnal choice
     # If the debug is enabled, it increase a lot the execution time. It is faster to
-    # print in this case in a file rather than in the stdout
-
-    # Block: file out.txt
-    with open("out.txt", "w") as f:
-        sys.stdout = f
+    # print in this case in a file rather than in the stdout.
+    if debug is True:
+        # Block: file out.txt
+        with open("out.txt", "w") as f:
+            sys.stdout = f
+            res, remaining = runner(items, items_len, uniques, debug)
+            print(f"For {sum(items) / limit} potential compos,")
+            print(
+                f"\n{len(res)} compos can be really obtained\nBatches are:\n{res}"
+                f"Remaining items are:\n{compute_values_from_indexes(items, remaining)}"
+            )
+            sys.stdout = original_stdout
+    else:
         res, remaining = runner(items, items_len, uniques, debug)
-        print(f"For {sum(items) / limit} potential compos,")
-        print(f"\n{len(res)} compos can be really obtained\nBatches are:\n{res}")
-        sys.stdout = original_stdout
-
-    # Block: stdout
-    # print(f"For {sum(items) / limit} potential compos,\n")
-    # print(
-    #     f"{len(res)} compos can be really obtained\nBatches are:\n{res}\n"
-    #     f"Remaining items are:\n{compute_values_from_indexes(items, remaining)}"
-    # )
+        print(f"For {sum(items) / limit} potential compos,\n")
+        print(
+            f"{len(res)} compos can be really obtained\nBatches are:\n{res}\n"
+            f"Remaining items are:\n{compute_values_from_indexes(items, remaining)}"
+        )
